73bcf7474ac6fc34942ae8899976966ae9e0b18a
Simón A Azócar <saimn.azocar@gmail.com>
2023-03-09 21:08:38 -0400

    Path alias in component/*

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import Layout from "components/Layout";
-import { AppProps } from "next/app";
+import Layout from '@components/Layout'
+import { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
