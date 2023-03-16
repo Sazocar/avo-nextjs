c5f053df8761acfe60445c1d1b72388ba1145894
Simón A Azócar <saimn.azocar@gmail.com>
2023-03-16 18:54:19 -0400

    DELETE title in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,6 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff"></meta>
-          <title>Avo Store | Simón A. Azócar</title>
         </Head>
         <body className="my-body-class">
           <Main />
