f9d23fc5260d7854b87f9e00f5941f0d419b6b5c
Simón A Azócar <saimn.azocar@gmail.com>
2023-03-16 21:11:52 -0400

    FIX ProductList grid

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Grid, Card, GridColumn } from 'semantic-ui-react'
 import Link from 'next/link'
 import Image from 'next/image'
 
@@ -9,32 +9,37 @@ type ProductListProps = {
 
 const mapProductsToCards = (products: TProduct[]) =>
   products.map(({ name, id, price, image }) => (
-    <Link
-      key={id}
-      href={`/product/${id}`}
-      passHref
-      legacyBehavior
-    >
-      <Card
-        as="a"
-        header={name}
-        image={
-          <Image src={image.toString()} priority width={230} height={230} alt={name.toString()} />
-        }
-        meta={<Card.Meta style={{ color: 'dimgray' }}>{price}</Card.Meta>}
-        style={{ width: '230px', height: 'auto' }}
-      />
-    </Link>
+    <Grid.Row>
+      <Grid.Column>
+        <Link key={id} href={`/product/${id}`} passHref legacyBehavior>
+          <Card
+            as="a"
+            header={name}
+            image={
+              <Image
+                src={image.toString()}
+                priority
+                width={230}
+                height={230}
+                alt={name.toString()}
+              />
+            }
+            meta={<Card.Meta style={{ color: 'dimgray' }}>{price}</Card.Meta>}
+            style={{ width: '230px', height: 'auto', margin: '12px' }}
+          />
+        </Link>
+      </Grid.Column>
+    </Grid.Row>
   ))
 
 const ProductList = ({ products }: ProductListProps) => (
-  <Card.Group
-    style={{ gap: '0px 30px', margin: '12px' }}
-    centered
-    doubling
-  >
-    {mapProductsToCards(products)}
-  </Card.Group>
+  <Grid container>
+    <Grid.Row>
+      <Card.Group centered doubling>
+        {mapProductsToCards(products)}
+      </Card.Group>
+    </Grid.Row>
+  </Grid>
 )
 
 export default ProductList
